2730cfd61e9ff1ea785a14869e7481de0725898a
Filipe Menezes <filipepmenezes@gmail.com>
2018-04-13 14:58:31 -0300

    Remove kiba-common/destinatinos/csv requirement.

--- lib/metamorfose.rb
+++ lib/metamorfose.rb
@@ -2,7 +2,6 @@ require 'metamorfose/version'
 
 require 'kiba'
 require 'kiba-common/sources/enumerable'
-require 'kiba-common/destinations/csv'
 
 require 'metamorfose/input/csv'
 require 'metamorfose/output/csv'
